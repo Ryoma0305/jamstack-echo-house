7feb7c1e68a50804365ede603e40d563b887086c
Ryoma0305 <ryomaaa0602@gmail.com>
2023-06-27 12:27:13 +0900

    wpデータ取得、表示

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import PostService from "../services/PostService";
+import PostType from "../types/PostType";
+
+const Home: NextPage<{
+  staticPostList: PostType[]; // 型の指定をする場所に注意！
+}> = ({ staticPostList }) => {
+  return (
+    <div>
+      {staticPostList.map((post) => {
+        return <p key={post.id}>{post.title}</p>; // 一個ずつ表示させる
+      })}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const staticPostList = await PostService.getList(); // postListをとってくる
+  return {
+    props: {
+      staticPostList,
+    },
+  };
+}
+
+export default Home;
