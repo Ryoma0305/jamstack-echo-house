89125fe440570b5595e5b4735cb5d0a14e1a0e05
Ryoma0305 <ryomaaa0602@gmail.com>
2023-07-24 18:02:19 +0900

    不要なインポートを削除

--- src/pages/post/[id]/index.tsx
+++ src/pages/post/[id]/index.tsx
@@ -1,7 +1,5 @@
 import Post from "@/components/post";
 import { usePost } from "@/hooks/usePost";
-import { useRouter } from "next/router";
-import useSWR from "swr";
 
 export const PostId = () => {
   const { post, postError, isLoading } = usePost();
