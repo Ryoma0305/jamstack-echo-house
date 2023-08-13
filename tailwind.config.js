29721abb0cfac1a21d5f635cbc2ea3db83931717
Ryoma0305 <ryomaaa0602@gmail.com>
2023-08-13 09:19:04 +0900

    tailwind cssをセットアップ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      body: ["Noto Sans JP", "sans-serif"],
+    },
+    screens: {
+      xs: "640px",
+      sm: "768px",
+      md: "896px",
+      lg: "960px",
+      xl: "1024px",
+      "2xl": "1280px",
+      "3xl": "1350px",
+      "4xl": "1400px",
+    },
     extend: {
-      fontFamily: {
-        body: ["Noto Sans JP", "sans-serif"],
+      colors: {
+        white: "#FFFFFF",
+        black: "#333333",
+        error: "#D14747",
+        link: "#297A89",
+        gray: {
+          100: "#F4F5F5",
+          200: "#EAEBEB",
+          300: "#D7DADA",
+          400: "#BFC3C4",
+          500: "#AFB5B6",
+          600: "#7A8385",
+          700: "#555C5D",
+          800: "#3D4243",
+          900: "#242525",
+        },
       },
     },
   },
